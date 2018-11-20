a3a1de7574ff71678b0216912d66eae6d742754a
dongdong.li <dongdong.li@gmkholdings.com>
2018-11-20 13:58:08 +0800

    修改测试

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/e',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
